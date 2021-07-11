ea6e5c371b705b83e63b382006dcd6e77d81ea70
Nima Poshtiban <poshtibannima@gmail.com>
2021-07-11 20:43:39 +0430

    Fomantic UI added + Header Component created and Added to app component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import Header from './Header';
 import StreamList from './streams/StreamList';
 import StreamCreate from './streams/StreamCreate';
 import StreamEdit from './streams/StreamEdit';
@@ -10,8 +11,9 @@ import NotFound from './errors/NotFound';
 
 const App = () => {
   return (
-    <div>
+    <div className="ui container">
       <Router>
+        <Header />
         <React.Fragment>
           <Switch>
             <Route exact path="/" component={StreamList} />
@@ -19,6 +21,7 @@ const App = () => {
             <Route exact path="/streams/edit" component={StreamEdit} />
             <Route exact path="/streams/delete" component={StreamDelete} />
             <Route exact path="/streams/show" component={StreamShow} />
+            {/* Add other routes above this Route , This route is for handling 404 page*/}
             <Route exact path="*" component={NotFound} />
           </Switch>
         </React.Fragment>
