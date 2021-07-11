7e3e67dd30188235dafc3a5b5963edb543be335f
Nima Poshtiban <poshtibannima@gmail.com>
2021-07-11 20:28:28 +0430

    Added Routhing & added basis components in stream folder

---
+++ client/src/components/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import StreamList from './streams/StreamList';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamShow from './streams/StreamShow';
+import NotFound from './errors/NotFound';
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <React.Fragment>
+          <Switch>
+            <Route exact path="/" component={StreamList} />
+            <Route exact path="/streams/new" component={StreamCreate} />
+            <Route exact path="/streams/edit" component={StreamEdit} />
+            <Route exact path="/streams/delete" component={StreamDelete} />
+            <Route exact path="/streams/show" component={StreamShow} />
+            <Route exact path="*" component={NotFound} />
+          </Switch>
+        </React.Fragment>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
