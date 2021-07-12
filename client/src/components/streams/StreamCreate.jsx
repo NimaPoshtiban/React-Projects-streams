9b5b88d90a641918dbe09ae88d2a96c3db8e2277
Nima Poshtiban <poshtibannima@gmail.com>
2021-07-12 19:24:06 +0430

    added redux-form to the project and used it inside StreamCreate component

--- client/src/components/streams/StreamCreate.jsx
+++ client/src/components/streams/StreamCreate.jsx
@@ -1,5 +1,57 @@
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
+import { Field, reduxForm } from 'redux-form';
+
+const StreamCreate = (props) => {
+  const renderError = ({ error, touched }) => {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  };
+
+  const renderInput = ({ input, label, meta }) => {
+    const className = `field ${meta.error && meta.touched? 'error':''}`
+    return (
+      <div className={className}>
+        <label>{label}</label>
+        <input autoComplete="off" {...input} />
+        {renderError(meta)}
+      </div>
+    );
+  };
+
+  const onSubmit = (formValues) => {
+    // this will be complete
+  };
+
+  return (
+    <form onSubmit={props.handleSubmit(onSubmit)} className="ui form error">
+      <Field label="Enter Title" name="title" component={renderInput} />
+      <Field
+        label="Enter Description"
+        name="description"
+        component={renderInput}
+      />
+      <button className="ui fluid button green">Submit</button>
+    </form>
+  );
+};
+// validating form data via redux-form
+const validate = (formValues) => {
+  const errors = {};
+  if (!formValues.title) {
+    errors.title = 'Title is required';
+  }
+  if (!formValues.description) {
+    errors.description = 'Description is required';
+  }
+  return errors;
 };
 
-export default StreamCreate;
+// docs at : https://redux-form.com
+export default reduxForm({
+  form: 'streamCreate',
+  validate,
+})(StreamCreate);
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 
 import authReducer from './authReducer';
 
 export default combineReducers({
   // Add reducers here
   auth: authReducer,
+  // Reducer for redux-form and form handling
+  form: formReducer,
 });
