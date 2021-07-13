e9b6703660dab5c6597896b056093e4c339c8afa
Nima Poshtiban <poshtibannima@gmail.com>
2021-07-13 17:53:08 +0430

    StreamList shows admin buttons if the user is logged in

--- client/src/components/streams/StreamList.jsx
+++ client/src/components/streams/StreamList.jsx
@@ -1,34 +1,71 @@
 import { useEffect } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
 import { fetchStreams } from '../../actions';
+
 const StreamList = (props) => {
   useEffect(() => {
     props.fetchStreams();
   }, []);
+  // decide if we should load Delete and Edit buttons
+  const renderAdmin = (stream) => {
+    if (stream.userId === props.currentUserId) {
+      return (
+        <div className="right floated content">
+          <button className="ui button primary">
+            <i className="icon edit" /> Edit
+          </button>
+          <button className="ui button negative">
+            <i className="trash alternate icon" />
+            Delete
+          </button>
+        </div>
+      );
+    }
+  };
 
-  const renderList=()=>{
-    return props.streams.map((stream)=>{
-      return(
+  const renderList = () => {
+    return props.streams.map((stream) => {
+      return (
         <div className="item" key={stream.id}>
-          <i className="large middle aligned icon camera"/>
-          <div className="content"> 
-          {stream.title}
-          </div>
-          <div className="description">
-            {stream.description}
+          {renderAdmin(stream)}
+
+          <i className="large middle aligned icon camera" />
+          <div className="content">
+            {stream.title}
+            <div className="description">{stream.description}</div>
           </div>
         </div>
-      )
-  })}
+      );
+    });
+  };
 
-  return <div>
-    <h2>Streams</h2>
-    <div className="ui celled list">
-      {renderList()}
+  const renderCreateButton = () => {
+    if (props.isSignedIn) {
+      return (
+        <div style={{ textAlign: 'right' }}>
+          <Link to="/streams/new" className="ui button green">
+            <i className="icon plus" /> Create Stream
+          </Link>
+        </div>
+      );
+    }
+  };
+
+  return (
+    <div>
+      <h2>Streams</h2>
+      <div className="ui celled list">{renderList()}</div>
+      {renderCreateButton()}
     </div>
-  </div>;
+  );
 };
 
-const mapStateToProps = (state) => ({ streams: Object.values(state.streams) });
+const mapStateToProps = (state) => ({
+  streams: Object.values(state.streams),
+  currentUserId: state.auth.userId,
+  isSignedIn: state.auth.isSignedIn,
+});
 
 export default connect(mapStateToProps, { fetchStreams })(StreamList);
