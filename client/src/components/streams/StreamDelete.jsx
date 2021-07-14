b724f967cd1d2800ea9b19e32d89e6d947f2e249
Nima Poshtiban <poshtibannima@gmail.com>
2021-07-14 12:39:04 +0430

    StreamDelete will show properly

--- client/src/components/streams/StreamDelete.jsx
+++ client/src/components/streams/StreamDelete.jsx
@@ -1,9 +1,27 @@
+import { Fragment } from 'react';
+
+import history from '../../history';
+import Modal from '../Modal';
+
 const StreamDelete = () => {
+  const actions = (
+    <Fragment>
+      <button className="ui button negative">Delete</button>
+      <button className="ui button">Cancel</button>
+    </Fragment>
+  );
+
   return (
     <div>
       StreamDelete
+      <Modal
+        title="Delete Stream"
+        content="Are you sure you want to delete this stream?"
+        actions={actions}
+        onDismiss={() => history.push('/')}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default StreamDelete
+export default StreamDelete;
